7904c9bd356674995672bf6a306bb784b402d3e9
neeleshrambally <ramballyn@gmail.com>
2022-09-22 17:35:28 +0200

    Removing malform pom

--- src/main/java/com/example/demo/utils/RestUtil.java
+++ src/main/java/com/example/demo/utils/RestUtil.java
@@ -1,16 +1,21 @@
 package com.example.demo.utils;
 
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 public class RestUtil {
 
     public static <T> Optional<T> get(RestTemplate template, String url, String bearerToken, Class<T> clazz) {
-        return get(template.exchange(url, HttpMethod.GET, null, clazz));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON }));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("User-Agent","Mozilla/5.0 Firefox/26.0");
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+        return get(template.exchange(url, HttpMethod.GET,entity, clazz));
     }
 
     private static <T> Optional<T> get(ResponseEntity<T> response) {
