33bc913a518e2aaeccb6d3c9b05b3936c037a681
neeleshrambally <ramballyn@gmail.com>
2022-09-22 17:49:32 +0200

    Removing malform pom

--- src/main/java/com/example/demo/configs/RestTemplateConfig.java
+++ src/main/java/com/example/demo/configs/RestTemplateConfig.java
@@ -5,10 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+
+        return builder.setConnectTimeout(Duration.ofSeconds(10000l)).setReadTimeout(Duration.ofSeconds(10000l)).build();
     }
 }
